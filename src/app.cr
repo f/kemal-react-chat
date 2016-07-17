f644dbc6ee10dd281e3128a07b938c8bbd54b2e7
Angarsk8 <andresa.garciah621@gmail.com>
2016-07-16 22:28:46 -0500

    add handler to on_close event to clean the list of sockets to avoid crashes on the app

--- src/app.cr
+++ src/app.cr
@@ -11,11 +11,19 @@ end
 
 ws "/" do |socket|
   sockets.push socket
+
+  # Handle incoming message and dispatch it to all connected clients
   socket.on_message do |message|
     messages.push message
     sockets.each do |a_socket|
       a_socket.send messages.to_json
     end
+  end
+
+  # Handle disconnection and clean sockets
+  socket.on_close do |_|
+    sockets.delete(socket)
+    puts "Closing Socket: #{socket}"
   end
 end
 
